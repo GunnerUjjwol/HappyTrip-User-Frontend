1620692655fdbc6de476fe6bc67e0ba3e1659e08
Ujjwol Dandekhya <i11136@d2hs.com>
2018-09-06 18:58:14 +0545

    Before Demo

--- src/app/packagespage/packagespage.component.ts
+++ src/app/packagespage/packagespage.component.ts
@@ -9,19 +9,22 @@ import { HttpClient } from '@angular/common/http';
 export class PackagespageComponent implements OnInit {
   list:any;
   user="Dummy Customer";
+  has_Booked='';
  
  
   constructor(private http:HttpClient) { }
 
   onBooked(l){
    
-    //const body={list:l,user:this.myVar};
-    //const body={PackageTrip:l,user:this.myVar};
+  
     const body={PackageTrip:l,user:this.user};
     console.log(body);
+    this.has_Booked='';
     let obs = this.http.post("http://localhost:8090/Booking/add",body); 
     obs.subscribe(()=>{});
   }
+
+
 
   ngOnInit() {
     let obs = this.http.get("http://localhost:8090/Package/all");
